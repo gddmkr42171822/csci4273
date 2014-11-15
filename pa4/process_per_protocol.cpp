07cf4276a45c668786ce0605045d5f5816128be9
horatiusMMXIV <rwghost98@gmail.com>
2014-11-15 02:15:14 -0700

    found bug; using my libraries now

--- pa4/process_per_protocol.cpp
+++ pa4/process_per_protocol.cpp
@@ -13,12 +13,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <iostream>
 
 #include "threadpool.h"
 
 #define BUFSIZE 4096
 
 void create_udp_socket(void *socket_type);
+
 
 using namespace std;
 
@@ -36,34 +38,35 @@ int main(int argc, char *argv[]) {
 	}
 	while (!th.thread_avail());
 	th.dispatch_thread(create_udp_socket, (void *)&in_udp_socket);
+
 	return 0;
 }
 
 void create_udp_socket(void *socket_type) {
-	struct sockaddr_in udpaddr;
+	struct sockaddr_in clientaddr;
 	struct hostent *phe;
 	int s, errno;
-	memset(&udpaddr, 0, sizeof(udpaddr));
-	udpaddr.sin_family = AF_INET;
-	udpaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	udpaddr.sin_port = htons(0);
+	memset(&clientaddr, 0, sizeof(clientaddr));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	clientaddr.sin_port = htons(0);
 	if((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		fprintf(stderr, "cannot creat UDP socket: %s\n", strerror(errno));
 	}
-	if(bind(s, (struct sockaddr *)&udpaddr, sizeof(udpaddr)) < 0) {
+	if(bind(s, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0) {
 		fprintf(stderr, "can't bind to port: %s\n", strerror(errno));
 		exit(1);
 	}
 	else {
-		socklen_t socklen = sizeof(udpaddr);
-		if((getsockname(s, (struct sockaddr *)&udpaddr, &socklen)) < 0) {
+		socklen_t socklen = sizeof(clientaddr);
+		if((getsockname(s, (struct sockaddr *)&clientaddr, &socklen)) < 0) {
 			fprintf(stderr, "getsockname error: %s\n", strerror(errno));
 		}
 		if((*(int *)socket_type) == 1) {
-			printf("New out udp socket port number is %d\n", ntohs(udpaddr.sin_port));
+			printf("New out udp socket port number is %d\n", ntohs(clientaddr.sin_port));
 		}
 		else {
-			printf("New in udp socket port number is %d\n", ntohs(udpaddr.sin_port));
+			printf("New in udp socket port number is %d\n", ntohs(clientaddr.sin_port));
 		}
 	}
 }
--- pa4/threadpool.h
+++ pa4/threadpool.h
@@ -1,43 +1,107 @@
-#ifndef __THREADPOOL_H
-#define __THREADPOOL_H
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <pthread.h>
-#include <mutex>
-#include <list>
-#include <condition_variable>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef threadpool_h
+#define threadpool_h
 
-#define DEFAULT_SIZE 10
+#include <vector>
+#include <queue>
+#include <errno.h>
+#include <cstring>
+#include <pthread.h>
 using namespace std;
 
-class ThreadPool
+class ThreadPool 
 {
-public:
-    ThreadPool();
-    ThreadPool(size_t threadCount);
-    ~ThreadPool();
-    int dispatch_thread(void dispatch_function(void*), void *arg);
-    bool thread_avail();
-private:
-    class job{
-        public:
-            job(void function(void*), void* arg){
-                this->function = function;
-                this->arg = arg;
-            }
-            void (*function)(void*);
-            void* arg;
-    };
-    pthread_t* threads;
-    bool terminate = false;
-    static void* execute(void* tp);
-    list<job*> jobs;
-    size_t threadCount;
-    mutex cvLock;
-    condition_variable cv;
-    size_t availableThreads;
+	public:
+		ThreadPool(size_t threadCount); // This is the constructor
+		~ThreadPool(); // This is the destructor
+		int dispatch_thread(void dispatch_function(void*), void *arg);
+		bool thread_avail();
+	private:
+		void *thread_work(void);
+		static void *work_helper(void *instance) {
+			return ((ThreadPool*)instance)->thread_work();
+		}
+		struct dispatch_struct {
+
+			void (*dispatch_function)(void*);
+			void *arg;
+
+		};
+		vector<pthread_t> active_threads;
+		queue<dispatch_struct*> workQueue;
+		pthread_mutex_t queue_mutex;
+		unsigned int threads_available;
+		bool program_is_over;
+		unsigned int max_threads;
 };
+
+ThreadPool::ThreadPool(size_t threadCount) 
+{
+	active_threads.resize(threadCount);
+	max_threads = threadCount;
+	threads_available = 0;
+	program_is_over = false;
+	pthread_mutex_init(&queue_mutex, NULL);
+	for(unsigned int i = 0; i < threadCount; i++) {
+		pthread_create(&active_threads[i], NULL, work_helper, this);
+	}
+}
+
+ThreadPool::~ThreadPool(void) 
+{
+	while(!workQueue.empty() && (threads_available < active_threads.size())) {
+		/* Wait until all the threads have completed their work */
+	}
+	program_is_over = true;
+	for(unsigned int i = 0; i < max_threads; i++) {
+		pthread_join(active_threads[i], NULL);
+	}
+	active_threads.clear();
+	pthread_mutex_destroy(&queue_mutex);
+}
+
+int ThreadPool::dispatch_thread(void dispatch_function(void*), void *arg) 
+{
+	dispatch_struct *thread_dispatch = new dispatch_struct;
+	thread_dispatch->arg = arg;
+	thread_dispatch->dispatch_function = dispatch_function;
+	workQueue.push(thread_dispatch);
+	return 0;
+}
+
+bool ThreadPool::thread_avail(void) 
+{
+	if(threads_available > workQueue.size() && workQueue.size() < max_threads) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void *ThreadPool::thread_work(void) 
+{
+	threads_available++;
+	while(1) {
+		if(pthread_mutex_trylock(&queue_mutex)) { 
+		}
+		else { 
+			threads_available--;
+			if(workQueue.size() > 0) {
+				void (*dispatch)(void*);
+				dispatch_struct *work;
+				work = workQueue.front();
+				workQueue.pop();
+				dispatch = work->dispatch_function;
+				dispatch(work->arg);
+				delete work;
+			}
+			pthread_mutex_unlock(&queue_mutex);
+			threads_available++;
+		}
+		if(program_is_over && workQueue.empty()) {
+			pthread_exit(NULL);	
+		}
+	}
+	return NULL;
+}
 #endif
